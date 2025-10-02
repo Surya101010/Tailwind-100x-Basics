12dba408c4f229e7a64b44a13a82104d525b1884
surya <greatuchsurya@gmail.com>
2025-10-02 17:24:00 +0530

    added OTP Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { OTP } from './components/OTP'
 
 function App() {
   
 
   return (
-    <>
-    <div className='grid grid-cols-12'>
-      <div className='col-span-12 bg-blue-600  sm:col-span-4'>
-        Hey inside blue div
-      </div><div className='col-span-12 bg-red-600 sm:col-span-6'>
-        Hey inside red div
-      </div><div className='col-span-12 bg-green-700 sm:col-span-2'>
-        Hey inside green div
-      </div>  
+    <div className='h-full bg-blue-300 '>
+      <div className='flex text-blue-500 p-32 justify-center'>
+        Webinar<div className='text-white'>.gg</div>
+      </div>
+      <div className=' flex justify-center py-4'>
+        Verify Your Age
+      </div>
+      <div className=' flex flex-col justify-center items-center mt-6'>
+        <p>Please Confirm your Birth Year</p>
+        <input type='integer' placeholder='Your Birth Year' name='Birth'></input>
+      </div>
+      <div className=' flex justify-center'>
+        <button>Continue</button>
+        </div>
+        <div className='flex justify-center'>
+          <OTP />
+        </div>
+        
     </div>
-    
-    </>
   )
 }
 
---
+++ src/components/OTP.jsx
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react";
+
+export function OTP(){
+    const ref1 = useRef();
+    const ref2= useRef();
+    const ref3 = useRef();
+    const ref4 = useRef();
+    const ref5 = useRef();
+    const ref6 = useRef();
+    const [disabled ,setDisabled]=useState(true);
+    return(
+        <div className="flex justify-center">   
+            <SubOtpBox reference={ref1} onDone={()=>{
+                ref2.current.focus();
+            }} />
+            <SubOtpBox reference={ref2} onDone={()=>{
+                ref3.current.focus();
+            }} />
+            <SubOtpBox reference={ref3} onDone={()=>{
+                ref4.current.focus();
+            }} />
+            <SubOtpBox reference={ref4} onDone={()=>{
+                ref5.current.focus();
+            }} />
+            <SubOtpBox reference={ref5} onDone={()=>{
+                ref6.current.focus();
+            }} />
+            <SubOtpBox reference={ref6} onDone={()=>{
+                setDisabled(false)
+            }} />
+        </div>
+    )
+}
+function SubOtpBox({
+    reference,onDone
+}){
+    return (
+        <div>
+            <input ref={reference} onChange={(e)=>{
+                onDone()
+            }} type="text" className='m-1 w-[40px] h-[50px] rounded-xl bg-blue-400 outline-none px-4 text-white' ></input>
+        </div>
+    )
+}
